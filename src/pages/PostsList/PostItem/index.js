5246c75708ec13fc62ab2696f05f1dd979a6228b
Tabachenko Danil <tabak7771@gmail.com>
2022-12-27 23:38:31 +0300

    add user name in post

--- src/pages/PostsList/PostItem/index.js
+++ src/pages/PostsList/PostItem/index.js
@@ -3,7 +3,7 @@ import styles from '../style.module.css';
 const PostItem = ({ post, userName }) => {
     return (
         <li>
-            <span className={styles.subtitle}><b>From User:</b> {userName.name}</span>
+            <span className={styles.subtitle}><b>From User:</b> {userName?.name}</span>
             <span className={styles.subtitle}><b>Title:</b> {post.title}</span>
             <span className={styles.subtitle}><b>Post:</b> {post.body}</span>
         </li>
